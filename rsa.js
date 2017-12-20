83d5b76250ba55649a14ba69c541aa70c0e62a5f
Alexander Kiselev <kiselev20060@gmail.com>
2017-12-20 19:54:21 +0300

    added decryption

--- rsa.js
+++ rsa.js
@@ -1,7 +1,6 @@
 var $encryptButton = document.getElementById("encrypt");
 var $decryptButton = document.getElementById("decrypt");
 
-
 var qInput = document.getElementById("q");
 var pInput = document.getElementById("p");
 var publicKeyInput = document.getElementById("pk");
@@ -9,8 +8,6 @@ var publicKeyInput = document.getElementById("pk");
 var sourceMessageInput = document.getElementById("source-message");
 var encryptedMessageInput = document.getElementById("encrypted-message");
 var decryptedMessageInput = document.getElementById("decrypted-message");
-
-
 
 function getPrivateKey(publicKey, pfi) {
 		for (var result = 2; result < pfi * pfi; result++) {
@@ -20,7 +17,6 @@ function getPrivateKey(publicKey, pfi) {
 		}
 		return NaN;
 }
-
 
 function encrypt(message, ko, n) {
 	var result = [];
@@ -39,8 +35,7 @@ function decrypt(message, ks, n) {
 }
 
 $encryptButton.addEventListener("click", function(e){
-	console.log("encrypt button clicked");
-
+	// console.log("encrypt button clicked");
 	var q = parseInt(qInput.value);
 	var p = parseInt(pInput.value);
 	var phi = (p - 1) * (q - 1);
@@ -50,32 +45,59 @@ $encryptButton.addEventListener("click", function(e){
 
 	var privateKey = getPrivateKey(publicKey, phi);
 
-
 	asciiRepr = [];
-	console.log(message)
+	// console.log(message)
 
 	for (var i = 0; i < message.length; i++) {
-		console.log(message[i])
+		// console.log(message[i])
 		 asciiRepr.push(message[i].charCodeAt(0));
 	}
 
-	encrypredAsciiRepr = encrypt(asciiRepr, publicKey, n);
+	var encrypredAsciiRepr = encrypt(asciiRepr, publicKey, n);
+	// console.log(encrypredAsciiRepr);
+	// console.log(encrypredAsciiRepr)
 
-	console.log(encrypredAsciiRepr)
+	val = "";
 
-	val = String.fromCharCode(...encrypredAsciiRepr);
+	for (var i = 0; i < encrypredAsciiRepr.length; i++) {
+		val += String.fromCharCode(encrypredAsciiRepr[i]);
+	}
 
-
-	console.log(val);
-
-
-
+	encryptedMessageInput.value = val;
 });
 
 $decryptButton.addEventListener("click", function(e){
-	console.log("decrypt button clicked");
+	// console.log("decrypt button clicked");
+	var q = parseInt(qInput.value);
+	var p = parseInt(pInput.value);
+	var phi = (p - 1) * (q - 1);
+	var publicKey = parseInt(publicKeyInput.value);
+	var message = encryptedMessageInput.value;
+	var n = q * p;
+
+	var privateKey = getPrivateKey(publicKey, phi);
+
+	asciiRepr = [];
+	// console.log(message)
+
+	for (var i = 0; i < message.length; i++) {
+		// console.log(message[i])
+		 asciiRepr.push(message[i].charCodeAt(0));
+	}
+
+	var decrypredAsciiRepr = decrypt(asciiRepr, privateKey, n);
+	// console.log(encrypredAsciiRepr);
+	// console.log(encrypredAsciiRepr)
+
+	console.log(decrypredAsciiRepr);
+
+	val = "";
+
+	for (var i = 0; i < decrypredAsciiRepr.length; i++) {
+		val += String.fromCharCode(decrypredAsciiRepr[i]);
+	}
+
+	decryptedMessageInput.value = val;
 });
 
-
-
-console.log("oj");
+// console.log("oj");
